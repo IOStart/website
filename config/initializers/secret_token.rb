40e1bc949e70158711faa584aa40b5ca01292ee7
Ignacio Ortega <ignacio.ortega@gmail.com>
2012-04-25 21:03:06 -0300

    add initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iostart::Application.config.secret_token = '83d89673bd34f46e521ee3ba958910d5adf861e000024337e50fe5e36c5887bdd6636e586eea3577d55b82ad0ec3b74ccf96e1fc610a0c14414161b50948c7cf'
